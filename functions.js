ddccf14ff08b21d58adf94875355e17832b100ff
IVBecy <57770911+IVBecy@users.noreply.github.com>
2020-12-25 22:12:42 +0000

    New card design and deleted usable app

--- functions.js
+++ functions.js
@@ -47,8 +47,6 @@ function Navbar(){
         <a href="about.html" id="/about.html"><h2>About me</h2></a>
         <a href="projects.html" id="/projects.html"><h2>Projects</h2></a>
         <a href="experience.html" id="/experience.html"><h2>Experiences</h2></a>
-        <a href="template.html" id="/template.html"><h2>Templates</h2></a>
-        <a href="apps.html" id="/apps.html"><h2>Apps</h2></a>
       </div>
     </div>
   )
@@ -65,137 +63,158 @@ else { document.getElementById(path).className = "active"}
 // Making the projects card
 if (path == "/projects.html"){
   const languages = {
-    py:"fab fa-python",
-    html:"fab fa-html5",
-    js:"fab fa-js-square",
-    php:"fab fa-php",
-    css:"fab fa-css3",
+    py: { name: "Python", colour: "#306998"},
+    html: { name: "HTML", colour: "#E44D26"},
+    js: { name: "Javascript", colour: "#F0DB4F" },
+    php: { name: "PHP", colour: "#787CB5" },
+    css: { name: "CSS", colour: "#3C99DC" },
   }
   const projects = {
     reaper: {
       title: "Reaper",
       date: "19 December 2020",
-      lang: languages["py"],
+      lang: languages["py"]["name"],
+      colour: languages["py"]["colour"],
       image: "images/reaper.png",
     },
     project_anon: {
       title: "Project anon",
       date: "10 December 2020",
-      lang: languages["php"],
+      lang: languages["php"]["name"],
+      colour: languages["php"]["colour"],
       image: "images/project_anon.png",
     },
     networkScan:{
       title:"Scanner",
       date:"16 September 2020",
-      lang:languages["py"],
+      lang: languages["py"]["name"],
+      colour: languages["py"]["colour"],
       image:"images/scanner.png",
     },
     reverseShell:{
       title:"Reverse Shell",
       date:"12 September",
-      lang:languages["py"],
+      lang: languages["py"]["name"],
+      colour: languages["py"]["colour"],
       image:"images/shell.png",
     },
     notepad:{
       title:"Notepad",
       date:"6 September 2020",
-      lang:languages["py"],
+      lang: languages["py"]["name"],
+      colour: languages["py"]["colour"],
       image:"images/notepad.png",
     },
     cardDesign:{
       title:"Card Designs",
       date:"24 August 2020",
-      lang:languages["css"],
+      lang: languages["css"]["name"],
+      colour: languages["css"]["colour"],
       image:"images/css_cards.png",
     },
     passManager:{
       title:"Password manager",
       date:"20 August 2020",
-      lang:languages["php"],
+      lang: languages["php"]["name"],
+      colour: languages["php"]["colour"],
       image:"images/php_pass.png",
     },
     speech:{
       title:"Speech recognition",
       date:"12 August 2020",
-      lang:languages["js"],
+      lang: languages["js"]["name"],
+      colour: languages["js"]["colour"],
       image:"images/speech.png",
     },
     usernameLookup:{
       title:"Spy.py",
       date:"10 August 2020",
-      lang:languages["py"],
+      lang: languages["py"]["name"],
+      colour: languages["py"]["colour"],
       image:"images/uname.png",
     },
     template3:{
       title:"Template (3)",
       date:"24 July 2020",
-      lang:languages["php"],
+      lang: languages["php"]["name"],
+      colour: languages["php"]["colour"],
       image:"images/design_3.png",
     },
     beatMaker:{
       title:"Beat Maker",
       date:"19 July 2020",
-      lang:languages["html"],
+      lang: languages["html"]["name"],
+      colour: languages["html"]["colour"],
       image:"images/beat.png",
     },
     phpForms:{
       title:"Forms",
       date:"18 July 2020",
-      lang:languages["js"],
+      lang: languages["php"]["name"],
+      colour: languages["php"]["colour"],
       image:"images/php_form.png",
     },
     todo:{
       title:"To-Do List",
       date:"21 June 2020",
-      lang:languages["js"],
+      lang: languages["js"]["name"],
+      colour: languages["js"]["colour"],
       image:"images/todo.png",
     },
     template2:{
       title:"Template (2)",
       date:"19 June 2020",
-      lang:languages["html"],
+      lang: languages["html"]["name"],
+      colour: languages["html"]["colour"],
       image:"images/design_2.png",
     },
     hashCracker:{
       title:"Hash Cracker",
       date:"12 June 2020",
-      lang:languages["py"],
+      lang: languages["py"]["name"],
+      colour: languages["py"]["colour"],
       image:"images/crack.png",
     },
     template1:{
       title:"Template (1)",
       date:"8 June 2020",
-      lang:languages["html"],
+      lang: languages["html"]["name"],
+      colour: languages["html"]["colour"],
       image:"images/design_1.png",
     },
     oldWebsite:{
       title:"Old Website",
-      date:"2 June 2020 (Retirement)",
-      lang:languages["html"],
+      date:"2 June 2020",
+      lang: languages["html"]["name"],
+      colour: languages["html"]["colour"],
       image:"images/old_website.png",
     },
     spaceInvaders:{
       title:"Space Invaders",
       date:"24 May 2020",
-      lang:languages["py"],
+      lang: languages["py"]["name"],
+      colour: languages["py"]["colour"],
       image:"images/space_invaders.png",
     },
     flappyBird:{
       title:"Flappy Bird",
       date:"4 April 2020",
-      lang:languages["js"],
+      lang: languages["js"]["name"],
+      colour: languages["js"]["colour"],
       image:"images/flappy_prev.png",
     },
     website:{
       title:"Website",
-      date:"2 February 2020 (Creation)",
-      lang:languages["html"],
+      date:"2 February 2020",
+      lang: languages["html"]["name"],
+      colour: languages["html"]["colour"],
       image:"images/prev.png",
     },
     snake:{
       title:"Snake",
       date:"16 September 2019",
-      lang:languages["py"],
+      lang: languages["py"]["name"],
+      colour: languages["py"]["colour"],
       image:"images/snake.png",
     }
   }
@@ -204,14 +223,12 @@ if (path == "/projects.html"){
     for (var i  in projects){
       const RenderingProcess = () => {
         list.push(
-        <div className="card" key={i} id={i}>
-          <h2>{projects[i].title}</h2>
-          <img id={"img".concat(i)} src={projects[i].image} style={{width:"70%"}} />
-          <h3>Date:</h3>
-          <p>{projects[i].date}</p>
-          <h3>Main language:</h3>
-          <i className={projects[i].lang} style={{fontSize:"35px"}}></i>
-        </div>
+          <div className="card" key={i}>
+            <img id={i} src={projects[i].image}/>
+            <div><h6 style={{ backgroundColor: projects[i].colour}}>{projects[i].lang}</h6><h5>{projects[i].date}</h5></div>
+            <div style={{ clear: "both"}}></div>
+            <h2>{projects[i].title}</h2>
+          </div>
         )
         return(list)
       }
@@ -223,7 +240,7 @@ if (path == "/projects.html"){
   // Image pop-up (modal)
   var imageDict = {}
   for(var index in list){
-    var imageSource  = document.getElementById("img".concat(list[index].key)).src
+    var imageSource = document.getElementById(list[index].key).src
     imageDict["img".concat(list[index].key)] = imageSource
   }
   $("img").click((e) => {
